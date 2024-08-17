e3b4f7e8c11eaf9249dd7e86c980d8eb206d80e1
zyberzebra <zyberzebra@users.noreply.github.com>
2024-08-17 08:49:25 +0200

    init

---
+++ letterboxdFeedFetcher.kts
@@ -0,0 +1,34 @@
+import java.net.URL
+import javax.xml.parsers.DocumentBuilderFactory
+import org.w3c.dom.Element
+import java.io.File
+
+fun main() {
+    val feedUrl = "https://letterboxd.com/zebrastuhl/rss/"
+    val rssFeed = URL(feedUrl).readText()
+    val document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        .parse(rssFeed.byteInputStream())
+
+    document.documentElement.normalize()
+
+    val items = document.getElementsByTagName("item")
+    val htmlContent = StringBuilder()
+    htmlContent.append("<html><head><title>Letterboxd RSS Feed</title></head><body>")
+    htmlContent.append("<h1>Recent Movies from Letterboxd</h1>")
+    htmlContent.append("<table border='1'><tr><th>Title</th><th>Link</th><th>Published</th></tr>")
+
+    for (i in 0 until items.length) {
+        val item = items.item(i) as Element
+        val title = item.getElementsByTagName("title").item(0).textContent
+        val link = item.getElementsByTagName("link").item(0).textContent
+        val pubDate = item.getElementsByTagName("pubDate").item(0).textContent
+
+        htmlContent.append("<tr><td>$title</td><td><a href='$link'>Link</a></td><td>$pubDate</td></tr>")
+    }
+
+    htmlContent.append("</table></body></html>")
+
+    // Speichern der HTML-Datei
+    val file = File("index.html")
+    file.writeText(htmlContent.toString())
+}
